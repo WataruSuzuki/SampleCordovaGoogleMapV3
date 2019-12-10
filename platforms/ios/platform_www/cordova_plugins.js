361e4d33c3b058d2a3040ad74350f32b72f395a3
WataruSuzuki <wataru0406@hotmail.co.jp>
2019-12-10 22:50:00 +0900

    Updated platform ios

--- platforms/ios/platform_www/cordova_plugins.js
+++ platforms/ios/platform_www/cordova_plugins.js
@@ -1,9 +1,6 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
-module.exports = [];
-module.exports.metadata = 
-// TOP OF METADATA
-{
+  module.exports = [];
+  module.exports.metadata = {
     "cordova-plugin-whitelist": "1.2.1"
-};
-// BOTTOM OF METADATA
+  };
 });
